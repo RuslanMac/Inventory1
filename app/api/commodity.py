5cd9f39a8a970b53c5d25eb6dc2ff574e52113f7
Ruslan <ruslan-karamurzanov@mail.ru>
2020-08-11 16:35:27 +0700

    queries fix

--- app/api/commodity.py
+++ app/api/commodity.py
@@ -50,20 +50,22 @@ def add_placement():
 	return jsonify({'answer': 'ok'  })
 
 @bp.route('/register', methods=['POST'])
+@cross_origin()
 def register():
-	password = request.form['password']
-	confirm_password = request.form['confirm_password']
+	row = request.get_json()
+	password = row['password']
+	confirm_password = row['confirm_password']
 	if password != confirm_password:
 		return error_response(400, "The Confirm Password confirmation does not match")
-	password_hash = Worker.set_password_hash(request.form['password'])
+	password_hash = Worker.set_password_hash(row['password'])
 
 
-	new_worker = Workers(login = request.form['login'], password_hash = password_hash)
+	new_worker = Workers(login = row['login'], password_hash = password_hash)
 	db.session.add(new_worker)
 	db.session.commit()
-	return jsonify({'request': password})
+	return jsonify({'request': 'ok'})
 
-'''@bp.route('/report/motion', methods=['POST'])
+@bp.route('/report/motion', methods=['POST'])
 def get_report_motion():
 	row = request.get_json()
 	oid_report = db.session.query(Objects).join(Movements).join(Divisions, Movements.division_id == Divisions.division_id).join(Operations).filter(Objects.oid == row['oid']).filter(Movements.operation_date.between(row['begin'], row['end'])).with_entities( Movements.operation_date, Movements.asinfo, Movements.description).all()        
@@ -95,9 +97,10 @@ def get_latest_info_object(oid):
 										FROM [Objects]
 										JOIN [Movement] ON [Objects].oid = [Movement].oid
 										JOIN [Division] ON [Division].division_id = [Movement].division_id
-										WHERE [Objects].oid = ?""",('a7a7e4e0-e986-4e65-b898-26308bb76a70'))
+										WHERE [Objects].oid = ?""",(oid))
+	object_latest = objects_latest.fetchone()
 	data = {
-		"object_id": objects_latest.items()[0], 
+		"object_id": object_latest[0], 
 		#"name": objects_latest[0],
 		#"discription": objects_latest.description,
 		#"barcode": objects_latest.barcode,
@@ -111,9 +114,38 @@ def get_latest_info_object(oid):
 	}
 	
 
-	return jsonify({'data': data})'''
+	return jsonify({'data': data})
 
 
+
+@bp.route('/object/new', methods=['POST'])
+@cross_origin()
+def add_new_objects():
+	row = request.get_json()
+	db.engine.execute("""INSERT INTO [Objects] ([object_Name], [description], [barcode])
+						VALUES (?, ?, ?)""", (row['object_Name'], row['description'] ,row['barcode'] ))
+	db.engine.execute("""INSERT INTO [Movement] ([operation_id], [division_id], [oid],     [description], [asinfo], [operation_date])
+						VALUES ((SELECT [Operation].operation_id
+								FROM [Operation]
+								WHERE [Operation].operation_Name = ?), 
+								(SELECT [Division].division_id
+								FROM [Division]
+								WHERE [Division].[Name] = ?),
+								(SELECT [Objects].[oid]
+								FROM [Objects]
+								WHERE [Objects].[object_Name]  = ?), ?, ?, ?)""", (row['operation'], row['division'], row['object_Name'],  row['description'], row['asinfo'], row['date']))
+	return jsonify({'data': 'ok'})
+
+
+@bp.route('/object/find', methods=['GET', 'POST'])
+@cross_origin()
+def get_object1():
+	row = request.get_json()
+	info = db.session.query.join(Objects, Objects.oid == Movements.oid)
+
+	records = info.all()
+
+	return jsonify({'data': records})
 
 
 
